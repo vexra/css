ad4cf796f8e7033ae950dd68095c86aa78bda030
vexra <unique123wowros@gmail.com>
2024-07-10 14:41:02 +0700

    feat: add login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -6,31 +6,38 @@ import { MdArrowBack } from 'react-icons/md'
 
 export default function Login() {
   return (
-    <main className="bg-gradient-css min-h-screen">
+    <main className="bg-gradient-css min-h-screen p-8">
       <Link href="/">
-        <MdArrowBack className="text-xl" />
+        <MdArrowBack className="text-3xl text-zinc-50" />
       </Link>
 
-      <h1 className="text-4xl font-bold text-zinc-50">Selamat Datang di CSS</h1>
-      <Image
-        src="/images/logo/computer-science-showdown.png"
-        alt="css logo"
-        width="360"
-        height="360"
-      />
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-2xl font-bold text-zinc-50 lg:text-4xl">
+          Selamat Datang di CSS
+        </h1>
+        <Image
+          src="/images/logo/computer-science-showdown.png"
+          alt="css logo"
+          width="360"
+          height="360"
+        />
 
-      <form action={login}>
-        <fieldset>
-          <legend className="text-3xl font-medium text-zinc-50">Log In</legend>
-          <button
-            type="submit"
-            className="rounded-lg bg-zinc-50 px-4 py-2 text-xl font-medium text-[#3A0CA3]"
-          >
-            <FcGoogle className="inline text-xl" />
-            Continue with Google
-          </button>
-        </fieldset>
-      </form>
+        <form className="flex flex-col" action={login}>
+          <div className="flex flex-col items-center justify-center gap-8">
+            <legend className="text-xl font-medium text-zinc-50 lg:text-3xl">
+              Log In
+            </legend>
+
+            <button
+              type="submit"
+              className="flex flex-row items-center gap-2 rounded-lg bg-zinc-50 px-4 py-2 text-xl font-medium text-[#3A0CA3]"
+            >
+              <FcGoogle className="text-xl lg:text-2xl" />
+              Continue with Google
+            </button>
+          </div>
+        </form>
+      </div>
     </main>
   )
 }
