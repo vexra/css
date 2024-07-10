5a479931eb18ee7de4e556e9935f25211202e9ce
Kira <117554978+kiraware@users.noreply.github.com>
2024-07-10 12:15:36 +0800

    create LCT description page

---
+++ src/app/(public)/lct/page.tsx
@@ -0,0 +1,167 @@
+import Link from 'next/link'
+
+export default function Ml() {
+  return (
+    <main className="p-4 sm:p-6 md:px-20 md:py-8 dark:bg-black">
+      <h1 className="mt-10 bg-gradient-heading bg-clip-text py-2 text-center text-5xl font-bold text-transparent">
+        Lomba Cepat Tepat
+      </h1>
+
+      <section className="mt-10">
+        <h2 className="text-4xl font-bold text-[#132D46] dark:text-slate-300">
+          Deskripsi Lomba
+        </h2>
+
+        <p className="mt-2 text-base text-slate-600 dark:text-slate-400">
+          Lomba Cepat Tepat Komputer adalah salah satu cabang lomba di bidang
+          akademik dalam rangkaian acara Computer Science Showdown tahun 2024.
+          Lomba ini diadakan untuk siswa/i SMA/MA/SMK Sederajat guna
+          mengembangkan keilmuan di bidang ilmu komputer. Materi yang dilombakan
+          dalam Lomba Cepat Tepat Komputer adalah mengenai pengetahuan dasar
+          dalam ilmu komputer seperti komunikasi data dan jaringan komputer,
+          Microsoft Office, pengetahuan umum dalam bidang komputer, sistem
+          bilangan, perangkat lunak dan perangkat keras komputer, dasar-dasar
+          sistem operasi, matematika diskrit, serta bahasa pemrograman
+          pseudocode. Peserta lomba ini, dituntut untuk cekatan, lugas, serta
+          tepat dalam menjawab pertanyaan-pertanyaan yang dilombakan.
+        </p>
+      </section>
+
+      <section className="mt-10">
+        <h2 className="text-4xl font-bold text-[#132D46] dark:text-slate-300">
+          Ketentuan Umum
+        </h2>
+
+        <p className="mt-2 text-base text-slate-600 dark:text-slate-400">
+          Ketentuan umum merupakan ketentuan yang berlaku untuk kategori
+          perlombaan Lomba Cepat Tepat
+        </p>
+
+        <ol className="mt-2 list-decimal px-6 marker:font-bold marker:text-[#3A0CA3]">
+          <li className="mt-2 text-base text-slate-600 dark:text-slate-400">
+            Peserta LCT Komputer adalah siswa/i yang duduk di kelas 1 - 3
+            SMA/MA/SMK/Sederajat.
+          </li>
+
+          <li className="mt-2 text-base text-slate-600 dark:text-slate-400">
+            Satu tim terdiri dari tiga orang.
+          </li>
+
+          <li className="mt-2 text-base text-slate-600 dark:text-slate-400">
+            Satu sekolah dapat mengirim lebih dari satu tim dengan maksimal 3
+            tim.
+          </li>
+
+          <li className="mt-2 text-base text-slate-600 dark:text-slate-400">
+            Satu tim didampingi oleh 1 orang guru pembimbing.
+          </li>
+        </ol>
+      </section>
+
+      <section className="mt-10">
+        <h2 className="text-4xl font-bold text-[#132D46] dark:text-slate-300">
+          Penghargaan
+        </h2>
+        <p className="mt-2 text-base text-slate-600 dark:text-slate-400">
+          Penghargaan bagi para pemenang Lomba Cepat Tepat Komputer
+        </p>
+
+        <ul className="mt-2 list-disc px-6 marker:font-bold marker:text-[#3A0CA3]">
+          <li className="mt-2 text-base text-slate-600 dark:text-slate-400">
+            Juara 1 : Uang Tunai senilai Rp. 900.000,00. + Sertifikat + Piala +
+            Voucher 50%
+          </li>
+
+          <li className="mt-2 text-base text-slate-600 dark:text-slate-400">
+            Juara 2 : Uang Tunai senilai Rp. 650.000,00. + Sertifikat + Piala +
+            Voucher 50%
+          </li>
+
+          <li className="mt-2 text-base text-slate-600 dark:text-slate-400">
+            Juara 3 : Uang Tunai senilai Rp. 400.000,00. + Sertifikat + Piala +
+            Voucher 50%
+          </li>
+          <li className="mt-2 text-base text-slate-600 dark:text-slate-400">
+            Seluruh peserta LCTK dan guru pembimbing akan mendapatkan
+            e-sertifikat
+          </li>
+        </ul>
+      </section>
+
+      <section className="mt-10">
+        <h2 className="text-4xl font-bold text-[#132D46] dark:text-slate-300">
+          Pendaftaran
+        </h2>
+
+        <p className="mt-2 text-base text-slate-600 dark:text-slate-400">
+          Timeline Lomba Cepat Tepat Komputer
+        </p>
+
+        <ul className="mt-2 list-disc px-6 marker:font-bold marker:text-[#3A0CA3]">
+          <li className="mt-2 text-base text-slate-600 dark:text-slate-400">
+            Gelombang I : 1 - 30 Agustus 2024
+          </li>
+
+          <li className="mt-2 text-base text-slate-600 dark:text-slate-400">
+            Gelombang II : 8 September - 8 Oktober 2024
+          </li>
+
+          <li className="mt-2 text-base text-slate-600 dark:text-slate-400">
+            Technical Meeting : 10 Oktober 2024
+          </li>
+
+          <li className="mt-2 text-base text-slate-600 dark:text-slate-400">
+            Pelaksanaan Lomba : 12 Oktober 2024
+          </li>
+        </ul>
+
+        <p className="mt-5 text-base text-slate-600 dark:text-slate-400">
+          Biaya pendaftaran Lomba Cepat Tepat Komputer sebesar
+        </p>
+
+        <ul className="mt-2 list-disc px-6 marker:font-bold marker:text-[#3A0CA3]">
+          <li className="mt-2 text-base text-slate-600 dark:text-slate-400">
+            Gelombang I : Rp. 155.000/tim (untuk 7 slot)
+          </li>
+
+          <li className="mt-2 text-base text-slate-600 dark:text-slate-400">
+            Gelombang I : Rp. 165.000/tim
+          </li>
+        </ul>
+
+        <p className="mt-5 text-base text-slate-600 dark:text-slate-400">
+          Biaya pendaftaran harus diberikan sebelum pendaftaran ditutup dengan
+          transfer melalui :
+        </p>
+
+        <ul className="mt-2 list-disc px-6 marker:font-bold marker:text-[#3A0CA3]">
+          <li className="mt-2 text-base text-slate-600 dark:text-slate-400">
+            Dana: 082289046489 a.n. Anindya Salsabila
+          </li>
+        </ul>
+      </section>
+
+      <section className="mt-10">
+        <h2 className="text-4xl font-bold text-[#132D46] dark:text-slate-300">
+          Contact Person
+        </h2>
+
+        <ul className="mt-2 list-disc px-6 marker:font-bold marker:text-[#3A0CA3]">
+          <li className="mt-2 text-base text-slate-600 dark:text-slate-400">
+            Luthfi Aditya : +62 812-1241-1730
+          </li>
+          <li className="mt-0 text-base text-slate-600 dark:text-slate-400">
+            Clara Monica : +62 822-6948-8323
+          </li>
+        </ul>
+      </section>
+
+      <Link
+        href="/registrasi-lct"
+        className="mx-auto mt-10 block w-fit rounded-lg bg-gradient-heading px-6 py-4 text-2xl font-semibold text-zinc-50 sm:px-8 md:px-16 md:text-4xl"
+      >
+        Daftar Sekarang
+      </Link>
+    </main>
+  )
+}
