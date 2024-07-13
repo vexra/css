c9523d2ece438daf78e89f9eb3f8df034725493d
Kira <117554978+kiraware@users.noreply.github.com>
2024-07-13 20:20:52 +0800

    update

--- src/app/(public)/pengumuman-lomba/page.tsx
+++ src/app/(public)/pengumuman-lomba/page.tsx
@@ -1,29 +1,29 @@
 export default function PengumumanLomba() {
   return (
-    <main className="bg-gradient-css flex flex-col items-center justify-center gap-8 px-4 py-8 font-sans">
-      <h1 className="text-4xl font-bold text-zinc-50">
+    <main className="bg-gradient-css flex flex-col items-center justify-center gap-8 px-4 py-8 font-sans md:px-8">
+      <h1 className="text-center text-3xl font-bold text-zinc-50 md:text-5xl">
         Pengumuman Juara Lomba
       </h1>
 
-      <section className="flex flex-col items-center justify-center">
+      <section className="flex w-full flex-col items-center justify-center gap-4 md:gap-8">
         {/* LCT */}
-        <div className="mb-6 rounded-lg bg-white shadow-lg dark:bg-gray-700">
-          <div className="rounded-t-lg bg-purple-100 px-6 py-4 dark:bg-gray-600">
-            <h3 className="text-lg font-bold text-purple-700 dark:text-gray-200">
+        <section className="w-full rounded-lg bg-zinc-50 shadow-lg dark:bg-purple-900">
+          <div className="rounded-t-lg bg-purple-100 px-6 py-4 dark:bg-purple-800">
+            <h2 className="text-2xl font-bold text-purple-700 md:text-4xl dark:text-zinc-100">
               Lomba Cepat Tepat
-            </h3>
+            </h2>
           </div>
 
-          <div className="p-6">
-            <table className="min-w-full divide-y divide-gray-200">
-              <thead className="bg-purple-300 dark:bg-gray-600">
-                <tr className="text-center align-middle dark:text-gray-200">
+          <div className="p-4 md:p-8">
+            <table className="min-w-full table-auto divide-y divide-purple-200">
+              <thead className="bg-purple-300 dark:bg-purple-800">
+                <tr className="text-center align-middle dark:text-zinc-100">
                   <th className="px-4 py-2">Juara</th>
                   <th className="px-4 py-2">Sekolah</th>
                   <th className="px-4 py-2">Nama</th>
                 </tr>
               </thead>
-              <tbody className="divide-y divide-gray-200 bg-white dark:text-gray-800">
+              <tbody className="divide-y divide-purple-200 bg-zinc-50 dark:text-slate-800">
                 <tr>
                   <td className="text-center align-middle" rowSpan={3}>
                     1
@@ -72,25 +72,25 @@ export default function PengumumanLomba() {
               </tbody>
             </table>
           </div>
-        </div>
+        </section>
 
         {/* CPC */}
-        <div className="mb-6 rounded-lg bg-white shadow-lg dark:bg-gray-700">
-          <div className="rounded-t-lg bg-purple-100 px-6 py-4 dark:bg-gray-600">
-            <h3 className="text-lg font-bold text-purple-700 dark:text-gray-200">
+        <section className="w-full rounded-lg bg-zinc-50 shadow-lg dark:bg-purple-900">
+          <div className="rounded-t-lg bg-purple-100 px-6 py-4 dark:bg-purple-800">
+            <h2 className="text-2xl font-bold text-purple-700 md:text-4xl dark:text-zinc-100">
               Competitive Programming Competition
-            </h3>
+            </h2>
           </div>
-          <div className="p-6">
-            <table className="min-w-full divide-y divide-gray-200">
-              <thead className="bg-purple-300 dark:bg-gray-600">
-                <tr className="text-center align-middle dark:text-gray-200">
+          <div className="p-4 md:p-8">
+            <table className="min-w-full table-auto divide-y divide-purple-200">
+              <thead className="bg-purple-300 dark:bg-purple-800">
+                <tr className="text-center align-middle dark:text-zinc-100">
                   <th className="px-4 py-2">Juara</th>
                   <th className="px-4 py-2">Instansi</th>
                   <th className="px-4 py-2">Nama</th>
                 </tr>
               </thead>
-              <tbody className="divide-y divide-gray-200 bg-white dark:text-gray-800">
+              <tbody className="divide-y divide-purple-200 bg-zinc-50 dark:text-slate-800">
                 <tr>
                   <td className="text-center align-middle" rowSpan={1}>
                     1
@@ -121,25 +121,25 @@ export default function PengumumanLomba() {
               </tbody>
             </table>
           </div>
-        </div>
+        </section>
 
         {/* Mobile Legend */}
-        <div className="mb-6 rounded-lg bg-white shadow-lg dark:bg-gray-700">
-          <div className="rounded-t-lg bg-purple-100 px-6 py-4 dark:bg-gray-600">
-            <h3 className="text-lg font-bold text-purple-700 dark:text-gray-200">
+        <div className="w-full rounded-lg bg-zinc-50 shadow-lg dark:bg-purple-900">
+          <div className="rounded-t-lg bg-purple-100 px-6 py-4 dark:bg-purple-800">
+            <h2 className="text-2xl font-bold text-purple-700 md:text-4xl dark:text-zinc-100">
               Mobile Legend
-            </h3>
+            </h2>
           </div>
-          <div className="p-6">
-            <table className="min-w-full divide-y divide-gray-200">
-              <thead className="bg-purple-300 dark:bg-gray-600">
-                <tr className="text-center align-middle dark:text-gray-200">
+          <div className="p-4 md:p-8">
+            <table className="min-w-full table-auto divide-y divide-purple-200">
+              <thead className="bg-purple-300 dark:bg-purple-800">
+                <tr className="text-center align-middle dark:text-zinc-100">
                   <th className="px-4 py-2">Juara</th>
                   <th className="px-4 py-2">Tim</th>
                   <th className="px-4 py-2">Nama</th>
                 </tr>
               </thead>
-              <tbody className="divide-y divide-gray-200 bg-white dark:text-gray-800">
+              <tbody className="divide-y divide-purple-200 bg-zinc-50 dark:text-slate-800">
                 <tr>
                   <td className="text-center align-middle" rowSpan={7}>
                     1
@@ -227,21 +227,21 @@ export default function PengumumanLomba() {
         </div>
 
         {/* Futsal */}
-        <div className="mb-6 rounded-lg bg-white shadow-lg dark:bg-gray-700">
-          <div className="rounded-t-lg bg-purple-100 px-6 py-4 dark:bg-gray-600">
-            <h3 className="text-lg font-bold text-purple-700 dark:text-gray-200">
+        <div className="w-full rounded-lg bg-zinc-50 shadow-lg dark:bg-purple-900">
+          <div className="rounded-t-lg bg-purple-100 px-6 py-4 dark:bg-purple-800">
+            <h2 className="text-2xl font-bold text-purple-700 md:text-4xl dark:text-zinc-100">
               Futsal
-            </h3>
+            </h2>
           </div>
-          <div className="p-6">
-            <table className="min-w-full divide-y divide-gray-200">
-              <thead className="bg-purple-300 dark:bg-gray-600">
-                <tr className="text-center align-middle dark:text-gray-200">
+          <div className="p-4 md:p-8">
+            <table className="min-w-full table-auto divide-y divide-purple-200">
+              <thead className="bg-purple-300 dark:bg-purple-800">
+                <tr className="text-center align-middle dark:text-zinc-100">
                   <th className="px-4 py-2">Juara</th>
                   <th className="px-4 py-2">Nama</th>
                 </tr>
               </thead>
-              <tbody className="divide-y divide-gray-200 bg-white dark:text-gray-800">
+              <tbody className="divide-y divide-purple-200 bg-zinc-50 dark:text-slate-800">
                 <tr>
                   <td className="text-center align-middle" rowSpan={1}>
                     1
