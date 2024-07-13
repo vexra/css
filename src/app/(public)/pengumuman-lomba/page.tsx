a09286ebf4e03d27c9a1973d5fd43d6c3b6a85e3
kiraware <117554978+kiraware@users.noreply.github.com>
2024-07-13 21:17:03 +0800

    Create Pengumuman Lomba Page (#27)
    Co-authored-by: Arkaan16 <muhammadfaruqarkaan16@gmail.com>

---
+++ src/app/(public)/pengumuman-lomba/page.tsx
@@ -0,0 +1,288 @@
+export default function PengumumanLomba() {
+  return (
+    <main className="bg-gradient-css flex flex-col items-center justify-center gap-8 px-4 py-8 font-sans md:px-8">
+      <h1 className="text-center text-3xl font-bold text-zinc-50 md:text-5xl">
+        Pengumuman Juara Lomba
+      </h1>
+
+      <section className="flex w-full flex-col items-center justify-center gap-4 md:gap-8">
+        {/* LCT */}
+        <section className="w-full rounded-lg bg-zinc-50 shadow-lg dark:bg-purple-900">
+          <div className="rounded-t-lg bg-purple-100 px-6 py-4 dark:bg-purple-800">
+            <h2 className="text-2xl font-bold text-purple-700 md:text-4xl dark:text-zinc-100">
+              Lomba Cepat Tepat
+            </h2>
+          </div>
+
+          <div className="p-4 md:p-8">
+            <table className="min-w-full table-auto divide-y divide-purple-200">
+              <thead className="bg-purple-300 dark:bg-purple-800">
+                <tr className="text-center align-middle dark:text-zinc-100">
+                  <th className="px-4 py-2">Juara</th>
+                  <th className="px-4 py-2">Sekolah</th>
+                  <th className="px-4 py-2">Nama</th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-purple-200 bg-zinc-50 dark:text-slate-800">
+                <tr>
+                  <td className="text-center align-middle" rowSpan={3}>
+                    1
+                  </td>
+                  <td className="text-center align-middle" rowSpan={3}>
+                    ?
+                  </td>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td className="text-center align-middle" rowSpan={3}>
+                    2
+                  </td>
+                  <td className="text-center align-middle" rowSpan={3}>
+                    ?
+                  </td>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td className="text-center align-middle" rowSpan={3}>
+                    3
+                  </td>
+                  <td className="text-center align-middle" rowSpan={3}>
+                    ?
+                  </td>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td>?</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </section>
+
+        {/* CPC */}
+        <section className="w-full rounded-lg bg-zinc-50 shadow-lg dark:bg-purple-900">
+          <div className="rounded-t-lg bg-purple-100 px-6 py-4 dark:bg-purple-800">
+            <h2 className="text-2xl font-bold text-purple-700 md:text-4xl dark:text-zinc-100">
+              Competitive Programming Competition
+            </h2>
+          </div>
+          <div className="p-4 md:p-8">
+            <table className="min-w-full table-auto divide-y divide-purple-200">
+              <thead className="bg-purple-300 dark:bg-purple-800">
+                <tr className="text-center align-middle dark:text-zinc-100">
+                  <th className="px-4 py-2">Juara</th>
+                  <th className="px-4 py-2">Instansi</th>
+                  <th className="px-4 py-2">Nama</th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-purple-200 bg-zinc-50 dark:text-slate-800">
+                <tr>
+                  <td className="text-center align-middle" rowSpan={1}>
+                    1
+                  </td>
+                  <td className="text-center align-middle" rowSpan={1}>
+                    ?
+                  </td>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td className="text-center align-middle" rowSpan={1}>
+                    2
+                  </td>
+                  <td className="text-center align-middle" rowSpan={1}>
+                    ?
+                  </td>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td className="text-center align-middle" rowSpan={1}>
+                    3
+                  </td>
+                  <td className="text-center align-middle" rowSpan={1}>
+                    ?
+                  </td>
+                  <td>?</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </section>
+
+        {/* Mobile Legend */}
+        <div className="w-full rounded-lg bg-zinc-50 shadow-lg dark:bg-purple-900">
+          <div className="rounded-t-lg bg-purple-100 px-6 py-4 dark:bg-purple-800">
+            <h2 className="text-2xl font-bold text-purple-700 md:text-4xl dark:text-zinc-100">
+              Mobile Legend
+            </h2>
+          </div>
+          <div className="p-4 md:p-8">
+            <table className="min-w-full table-auto divide-y divide-purple-200">
+              <thead className="bg-purple-300 dark:bg-purple-800">
+                <tr className="text-center align-middle dark:text-zinc-100">
+                  <th className="px-4 py-2">Juara</th>
+                  <th className="px-4 py-2">Tim</th>
+                  <th className="px-4 py-2">Nama</th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-purple-200 bg-zinc-50 dark:text-slate-800">
+                <tr>
+                  <td className="text-center align-middle" rowSpan={7}>
+                    1
+                  </td>
+                  <td className="text-center align-middle" rowSpan={7}>
+                    ?
+                  </td>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td className="text-center align-middle" rowSpan={7}>
+                    2
+                  </td>
+                  <td className="text-center align-middle" rowSpan={7}>
+                    ?
+                  </td>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td className="text-center align-middle" rowSpan={7}>
+                    3
+                  </td>
+                  <td className="text-center align-middle" rowSpan={7}>
+                    ?
+                  </td>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td>?</td>
+                </tr>
+                <tr>
+                  <td>?</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+
+        {/* Futsal */}
+        <div className="w-full rounded-lg bg-zinc-50 shadow-lg dark:bg-purple-900">
+          <div className="rounded-t-lg bg-purple-100 px-6 py-4 dark:bg-purple-800">
+            <h2 className="text-2xl font-bold text-purple-700 md:text-4xl dark:text-zinc-100">
+              Futsal
+            </h2>
+          </div>
+          <div className="p-4 md:p-8">
+            <table className="min-w-full table-auto divide-y divide-purple-200">
+              <thead className="bg-purple-300 dark:bg-purple-800">
+                <tr className="text-center align-middle dark:text-zinc-100">
+                  <th className="px-4 py-2">Juara</th>
+                  <th className="px-4 py-2">Nama</th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-purple-200 bg-zinc-50 dark:text-slate-800">
+                <tr>
+                  <td className="text-center align-middle" rowSpan={1}>
+                    1
+                  </td>
+                  <td className="text-center">?</td>
+                </tr>
+                <tr>
+                  <td className="text-center align-middle" rowSpan={1}>
+                    2
+                  </td>
+                  <td className="text-center">?</td>
+                </tr>
+                <tr>
+                  <td className="text-center align-middle" rowSpan={1}>
+                    3
+                  </td>
+                  <td className="text-center">?</td>
+                </tr>
+                <tr>
+                  <td className="text-center align-middle" rowSpan={1}>
+                    Best Player
+                  </td>
+                  <td className="text-center">?</td>
+                </tr>
+                <tr>
+                  <td className="text-center align-middle" rowSpan={1}>
+                    Top Scorer
+                  </td>
+                  <td className="text-center">?</td>
+                </tr>
+                <tr>
+                  <td className="text-center align-middle" rowSpan={1}>
+                    Best Suporter
+                  </td>
+                  <td className="text-center">?</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </section>
+    </main>
+  )
+}
